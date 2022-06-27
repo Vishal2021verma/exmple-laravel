b26b59667d561f94ee52d9cd4bf887ab3b1a4014
Vishal Verma <black2020panther@gmail.com>
2022-06-27 15:10:02 +0530

    first commit

---
+++ app/Http/Controllers/ToDoListController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ListItem;
+
+use Illuminate\Support\Facades\Log;
+
+class ToDoListController extends Controller
+{
+    public function index(){
+        return view('welcome', ['listItems' => ListItem::all()]);
+    }
+    //
+    public function saveItem(Request $request){
+       Log::info(json_encode($request->all()));
+       $newListItem = new ListItem;
+       $newListItem->name = $request->listItem;
+       $newListItem->is_complete = 0;
+      
+       $newListItem->save();
+        return redirect('/');
+    } 
+}
